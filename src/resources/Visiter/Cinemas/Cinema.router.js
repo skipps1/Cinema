882eca7b48a47bed4dc0d1d23aaa62e70b36cf6e
skipps1 <valik200511@gmail.com>
2023-04-12 22:05:12 +0300

    feat: done 2 routers

--- src/resources/Visiter/Cinemas/Cinema.router.js
+++ src/resources/Visiter/Cinemas/Cinema.router.js
@@ -1,14 +1,83 @@
 import { Router } from 'express';
 
-import User from './user.model.js';
-import * as usersService from './user.service.js';
+import Cinema from './Cinema.service.js';
+import * as cinemaService from './Cinema.service.js';
 
 const router = Router();
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+router.route('/').get(
+  catchErrors(async (req, res) => {
+    const cinema = await cinemaService.getAll();
+
+    res.json(cinema.map(Cinema.toResponse));
+  })
+);
+
+router.route('/').post(
+  catchErrors(async (req, res) => {
+    const { id, Adress, Num_Of_Halls } = req.body;
+
+    const cinema = await cinemaService.createUser({ id, Adress, Num_Of_Halls });
+
+    if (cinema) {
+      res.status(StatusCodes.CREATED).json(Cinema.toResponse(cinema));
+    } else {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ code: 'CINEMA_NOT_CREATE', msg: 'Cinema not create' });
+    }
+  })
+);
+
+router.route('*связка*').get(
+  catchErrors(async (req, res) => {
+    const { ID } = req.params;
+
+    const cinema = await cinemaService.getById(id);
+
+    if (cinema) {
+      res.json(Cinema.toResponse(cinema));
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'CINEMA_NOT_FOUND', msg: 'Cinema not found' });
+    }
+  })
+);
+
+router.route('*связка*').put(
+  catchErrors(async (req, res) => {
+    const { ID } = req.params;
+    const { id, Adress, Num_of_Halls } = req.body;
+
+    const cinema = await cinemaService.updateById({ ID, id, Adress, Num_of_Halls });
+
+    if (cinema) {
+      res.status(StatusCodes.OK).json(Cinema.toResponse(cinema));
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'CINEMA_NOT_FOUND', msg: 'Cinema not found' });
+    }
+  })
+);
+
+router.route('*связка*').delete(
+  catchErrors(async (req, res) => {
+    const { ID } = req.params;
+
+    const cinema = await cinemaService.deleteById(id);
+
+    if (!cinema) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'CINEMA_NOT_FOUND', msg: 'Cinema not found' });
+    }
+
+    return res
+      .status(StatusCodes.NO_CONTENT)
+      .json({ code: 'CINEMA_DELETED', msg: 'The cinema has been deleted' });
+  })
+);
 
 export default router;
--- src/resources/Visiter/Visiters/Visiter.router.js
+++ src/resources/Visiter/Visiters/Visiter.router.js
@@ -1,14 +1,85 @@
 import { Router } from 'express';
-
-import User from './user.model.js';
-import * as usersService from './user.service.js';
+import catchErrors from '../../../common/catchErrors.js';
+import Visiter from './Visiter.memory.repository.js';
+import * as visitersService from './Visiter.service.js';
 
 const router = Router();
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+
+
+router.route('/').get(
+  catchErrors(async (req, res) => {
+    const visiters = await visitersService.getAll();
+
+    res.json(visiters.map(Visiter.toResponse));
+  })
+);
+
+router.route('/').post(
+  catchErrors(async (req, res) => {
+    const { id, Name, Age } = req.body;
+
+    const visiter = await visitersService.createVisiter({ id, Name, Age });
+
+    if (visiter) {
+      res.status(StatusCodes.CREATED).json(Visiter.toResponse(visiter));
+    } else {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ code: 'VISITER_NOT_CREATE', msg: 'Visiter not create' });
+    }
+  })
+);
+
+router.route('/:id').get(
+  catchErrors(async (req, res) => {
+    const { id } = req.params;
+
+    const visiter = await visitersService.getById(id);
+
+    if (visiter) {
+      res.json(Visiter.toResponse(visiter));
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: ' VISITER_NOT_FOUND', msg: 'Visiter not found' });
+    }
+  })
+);
+
+router.route('/:id').put(
+  catchErrors(async (req, res) => {
+    const { id } = req.params;
+    const { title, columns } = req.body;
+
+    const board = await boardsService.updateById({ id, title, columns });
+
+    if (board) {
+      res.status(StatusCodes.OK).json(Board.toResponse(board));
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'BOARD_NOT_FOUND', msg: 'Board not found' });
+    }
+  })
+);
+
+router.route('/:id').delete(
+  catchErrors(async (req, res) => {
+    const { id } = req.params;
+
+    const visiter = await visitersService.deleteById(id);
+
+    if (!visiter) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'VISITER_NOT_FOUND', msg: 'Visiter not found' });
+    }
+
+    return res
+      .status(StatusCodes.NO_CONTENT)
+      .json({ code: 'VISITER_DELETED', msg: 'Visiter has been deleted' });
+  })
+);
 
 export default router;
